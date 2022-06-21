37108cbfd390f61e76f3d4f697c7d4c851a880fb
Naime Medjek <naimemedjek@gmail.com>
2022-06-21 16:33:40 +0200

    js script done

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-let meter = document.getElementById("meter")
-let feet = document.getElementById("feet")
-let liter = document.getElementById("liter")
-let gallon = document.getElementById("gallon")
-let kilo = document.getElementById("kilo")
-let pound = document.getElementById("pound")
+const meter = document.getElementById("meter")
+const feet = document.getElementById("feet")
+const liter = document.getElementById("liter")
+const gallon = document.getElementById("gallon")
+const kilo = document.getElementById("kilo")
+const pound = document.getElementById("pound")
 
+const meterToFeet = document.getElementById("meterToFeet")
+const feetToMeter = document.getElementById("feetToMeter")
+
+const literToGallon = document.getElementById("literToGallon")
+const gallonToLiter = document.getElementById("gallonToLiter")
+
+const kiloToPound = document.getElementById("kiloToPound")
+const poundToKilo = document.getElementById("poundToKilo")
+
+const input = document.getElementById("input")
+const btn = document.getElementById("btn")
+
+
+btn.addEventListener("click", function(){
+    meter.textContent = input.value
+    feet.textContent = input.value
+    liter.textContent = input.value
+    gallon.textContent = input.value
+    kilo.textContent = input.value
+    pound.textContent = input.value
+
+    meterToFeet.textContent = (input.value * 3.281).toFixed(3)
+    feetToMeter.textContent = (input.value / 3.281).toFixed(3)
+    literToGallon.textContent = (input.value * 0.264).toFixed(3)
+    gallonToLiter.textContent = (input.value / 0.264).toFixed(3)
+    kiloToPound.textContent = (input.value  * 2.204).toFixed(3)
+    poundToKilo.textContent = (input.value / 2.204).toFixed(3)
+})
