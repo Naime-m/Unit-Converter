478f7533d41fdddf9592552ea5e9f011e902a8fd
Naime Medjek <naimemedjek@gmail.com>
2022-06-21 16:06:22 +0200

    html base + declare js var

---
+++ script.js
@@ -0,0 +1,7 @@
+let meter = document.getElementById("meter")
+let feet = document.getElementById("feet")
+let liter = document.getElementById("liter")
+let gallon = document.getElementById("gallon")
+let kilo = document.getElementById("kilo")
+let pound = document.getElementById("pound")
+
